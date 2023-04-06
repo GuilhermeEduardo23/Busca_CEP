6d0cfeb2015026d0ca853c53b8e37f2a561693b1
GuilhermeEduardo23 <guilhermeeduardo-17@outlook.com>
2023-04-06 08:23:59 -0300

    Término do HTML e construção do CSS

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+const bodyWidth = document.querySelector(`body`).clientWidth,
+    bodyHeight = document.querySelector(`body`).clientHeight,
+    inputSearch = document.getElementById(`input-search`),
+    btnSearch = document.getElementById(`search`);
+
+btnSearch.addEventListener(`click`, () => {
+    
+    console.log(inputSearch);
+});
